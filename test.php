26b56cdb74902125ada4e2e9332fb27aa481a112
Toni Hermoso Pulido <toniher@cau.cat>
2017-04-15 22:53:04 +0200

    testing datavalues

--- test.php
+++ test.php
@@ -10,6 +10,11 @@ $api = new MwApi\MediawikiApi( "https://www.wikidata.org/w/api.php" );
 $dataValueClasses = array(
     'unknown' => 'DataValues\UnknownValue',
     'string' => 'DataValues\StringValue',
+    'boolean' => 'DataValues\BooleanValue',
+    'number' => 'DataValues\NumberValue',
+    'time' => 'DataValues\TimeValue',
+    'globecoordinate' => 'DataValues\Geo\Values\GlobeCoordinateValue',
+    'wikibase-entityid' => 'Wikibase\DataModel\Entity\EntityIdValue',
 );
 
 $wbFactory = new WbApi\WikibaseFactory(
@@ -22,6 +27,17 @@ $wbFactory = new WbApi\WikibaseFactory(
 $revision = $wbFactory->newRevisionGetter()->getFromId( 'Q16943393' );
 $item = $revision->getContent()->getData();
 
-var_dump( $item );
+#var_dump( $item );
+
+$statementList = $item->getStatements();
+
+# ./vendor/wikibase/data-model/src/Statement/StatementList.php
+
+$propIds = $statementList->getPropertyIds();
+
+var_dump( array_keys( $propIds ) );
+
+var_dump( $statementList );
+
 
 $api->logout();
