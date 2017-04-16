0e8407db960d4b7dc1f3d45b53afdca21f375a60
Toni Hermoso Pulido <toniher@cau.cat>
2017-04-16 20:05:44 +0200

    update id, snack

--- premirebut.php
+++ premirebut.php
@@ -5,6 +5,8 @@ require_once( __DIR__ . '/vendor/autoload.php' );
 use \Mediawiki\Api as MwApi;
 use \Wikibase\Api as WbApi;
 
+use League\Csv\Reader;
+
 // Detect commandline args
 $conffile = 'config.json';
 $csvfile = 'list.csv';
@@ -22,7 +24,21 @@ if ( ! file_exists( $conffile ) || ! file_exists( $csvfile ) ) {
 	die( "Files needed" );
 }
 
-$api = new MwApi\MediawikiApi( "https://www.wikidata.org/w/api.php" );
+
+$confjson = file_get_contents( $conffile );
+
+$wikiconfig = null;
+$wikidataconfig = null;
+
+if ( array_key_exists( "wikipedia", $confjson ) ) {
+	$wikiconfig = $confjson["wikipedia"];
+}
+
+if ( array_key_exists( "wikidata", $confjson ) ) {
+	$wikidataconfig = $confjson["wikidata"];
+}
+
+$api = new MwApi\MediawikiApi( $wikidataconfig['url'] );
 
 $dataValueClasses = array(
     'unknown' => 'DataValues\UnknownValue',
@@ -40,29 +56,116 @@ $wbFactory = new WbApi\WikibaseFactory(
     new DataValues\Serializers\DataValueSerializer()
 );
 
-// Process CSV List, ignore first row
 
-// URL detect
-// https://ca.wikipedia.org/w/api.php?action=query&titles=Amical_Wikimedia&prop=wbentityusage
-// If page exists,
-// If not, then redirect ...
-// if entity exists of the type... then...
+$reader = Reader::createFromPath( $csvfile );
 
+$reader->setOffset(1);
+$reader->setDelimiter("\t");
 
-$revision = $wbFactory->newRevisionGetter()->getFromId( 'Q16943393' );
-$item = $revision->getContent()->getData();
+$results = $reader->fetch();
 
-#var_dump( $item );
-
-$statementList = $item->getStatements();
-
-# ./vendor/wikibase/data-model/src/Statement/StatementList.php
-
-$propIds = $statementList->getPropertyIds();
-
-var_dump( array_keys( $propIds ) );
-
-var_dump( $statementList );
-
+foreach ( $results as $row ) {
+	
+	echo $row[0]."\n";
+	
+	$wdid = retrieveWikidataId( $row[0] );
+	
+	if ( $wdid ) {
+		// Add statement and ref
+		addStatement( $wbFactory, $wdid, $row, $wikiconfig );
+	}
+	
+}
 
 $api->logout();
+
+function retrieveWikidataId( $title, $wikiconfig ){
+
+	$wdid = null;
+	
+	$title = str_replace( " ", "_", $title );
+	
+	$url = $wikiconfig["url"]."?action=query&prop=wbentityusage&titles=".$title;
+	
+	// Process url
+	$json = file_get_contents( $url );
+	
+	// Proceess JSON
+	$obj = json_decode( $json, true );
+	
+	if ( array_key_exists( "query", $obj ) ) {
+
+		if ( array_key_exists( "pages", $obj['query'] ) ) {
+
+			// Assume first key
+			foreach ( $obj['query']["pages"] as $key => $struct ) {
+				
+				if ( array_key_exists( "wbentityusage", $struct ) ) {
+					
+					$wdid = retrieveWikidataIdfromStruct( $struct );
+					
+				}
+				
+			}
+		}
+	}
+	
+	return $wdid;
+}
+
+function retrieveWikidataIdfromStruct( $struct ){
+	
+	$wikidataid = null;
+	
+	foreach ( $struct as $key => $hash ) {
+		
+		if ( array_key_exists( "aspects", $hash ) ) {
+					
+			if ( in_array( "O", $hash["aspects"] ) && in_array( "S", $hash["aspects"] ) ) {
+				$wikidataid = $key;
+			}
+		}	
+	}
+	
+	return $wikidataid;
+}
+
+function addStatement( $wbFactory, $id, $row, $wikiconfig ){
+	
+	$saver = $wbFactory->newRevisionSaver();
+	
+	$revision = $wbFactory->newRevisionGetter()->getFromId( $id );
+	$item = $revision->getContent()->getData();
+	$statementList = $item->getStatements();
+
+	$propId = 1320;
+	if ( array_key_exists( 1, $row ) ) {
+		
+		$award = $row[1];
+		$date = null;
+		$ref = null;
+		
+		if ( array_key_exists( 2, $row ) ) {
+			$date = $row[2];
+		}
+
+		if ( array_key_exists( 3, $row ) ) {
+			$ref = $row[3];
+		}
+		
+		if( $statementList->getByPropertyId( PropertyId::newFromNumber( $propId ) )->isEmpty() ) {
+			$statementCreator->create(
+				new PropertyValueSnak(
+					PropertyId::newFromNumber( $propId ),
+					PropertyId::newFromNumber( retrieveWikidataId( $award, $wikiconfig ) )
+				),
+				$id
+			);
+		} else {
+			echo "Ignore for ".$id."\n";
+		}
+		
+		$saver->save( $revision );
+	
+	}
+}
