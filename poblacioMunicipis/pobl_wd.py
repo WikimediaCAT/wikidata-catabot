cbe474b16fce409031a156b976081cfe23526de5
Joan Creus Andreu <joan.creusandreu@gmail.com>
2018-12-06 22:40:20 +0100

    canvis en el format de wikidata, posar "preferred" als fogatges, ...

--- poblacioMunicipis/pobl_wd.py
+++ poblacioMunicipis/pobl_wd.py
@@ -4,6 +4,21 @@ import codecs
 import re
 import csv
 
+def cmp_data(wbtim,anny,mes,dia):
+   if wbtim.year > anny:
+      return 1
+   if wbtim.year < anny:
+      return -1
+   if wbtim.month > mes:
+      return 1
+   if wbtim.month < mes:
+      return -1
+   if wbtim.day < dia:
+      return 1
+   if wbtim.day < dia:
+      return -1
+   return 0
+
 class WDint:
     """ Interfície amb wikidata                     """
     """ wdsite: Site de wikidata                    """
@@ -20,8 +35,7 @@ class WDint:
 
       # Ara, la URL de la referència
       urldelaref = pywikibot.Claim(self.repowd,'P854')
-      urldelaref.setTarget("http://ced.uab.cat/catalonia/evolucio/Poblacio%20de%20fet.zip")
-      lrefs=[urldelaref]
+      urldelaref.setTarget("http://ced.uab.es/infraestructures/banc-de-dades-espanya-i-catalunya/evolucio-i-continuitat-dels-municipis-catalans-1497-2002/")
 
       # Ara, la data, amb precisió d'any
       if any_cens<= 1582:
@@ -34,13 +48,39 @@ class WDint:
 
       # Un altre qualificador, mètode de determinació ('P459')
       qualif2 = pywikibot.Claim(self.repowd,'P459')
-      # el mètode és el padró municipal d'habitants, o Q745221
-      qualif2.setTarget(pywikibot.ItemPage(self.repowd,"Q745221"))
+      if propietat == 'P1082':
+        # el mètode és el padró municipal d'habitants, o Q745221 si és població
+        qualif2.setTarget(pywikibot.ItemPage(self.repowd,"Q745221"))
+      elif propietat == 'P1538':
+        # el mètode és el fogatge, o Q2361901 si és nombre de llars
+        qualif2.setTarget(pywikibot.ItemPage(self.repowd,"Q2361901"))
+      else:
+        print u'Propietat incorrecta a wd_actualitzar_pob',propietat
+
+      # Ara la referència "Editorial" ('P123') a Centre d'Estudis Demogràfics
+      ref2 = pywikibot.Claim(self.repowd,'P123')
+      ref2.setTarget(pywikibot.ItemPage(self.repowd,"Q25892981"))   # CED
+
+      # una altra referència: data de consulta (P813) = 25-11-19
+      data_consulta = pywikibot.WbTime(year=2018,month=11,day=25,precision='day')
+      data_item = pywikibot.Claim(self.repowd,'P813')
+      # construim l'item
+      data_item.setTarget(data_consulta)
+
+      # Construïm la llista de referències
+      lrefs=[data_item,ref2,urldelaref]
 
       self.item.addClaim(pob_clm)
       pob_clm.addQualifier(qualif)
       pob_clm.addQualifier(qualif2)
       pob_clm.addSources(lrefs)
+
+    def wd_nom_municipi(self):
+      try:
+        toret = self.item.labels['ca']
+      except:
+        toret = u"La Label del municipi no existeix!!"
+      return toret
 
     def wd_anys_amb_poblacio(self,propietat):
       toret = set()
@@ -87,6 +127,44 @@ class WDint:
       if not municat or muniesp:
         print u"Atenció, "+self.item.labels['ca']+" ("+self.item.id+u") té anomalia a la P31"
 
+    def wd_posar_preferit(self,propietat):
+       if propietat in self.item.claims:
+         llista = self.item.claims[propietat]
+         max_any = -10000
+         max_mes = 1
+         max_dia = 1
+         n_anys = 0
+         error_indeterminat = False
+         algun_preferred = False
+         preferits = []
+         for clm in llista:
+            n_anys = n_anys + 1
+            wdestad = clm.getTarget()
+            wdrank = clm.getRank()
+            if wdrank == "preferred":
+               algun_preferred = True
+            try:
+               wdqual = clm.qualifiers['P585']
+            except KeyError:
+               wdqual = []
+               error_indeterminat = True
+            for p in wdqual:
+               tgttime = p.getTarget()
+               if cmp_data(tgttime,max_any,max_mes,max_dia)>0:
+                   max_any = tgttime.year
+                   max_mes = tgttime.month
+                   max_dia = tgttime.day
+                   clmbo = clm
+
+         if algun_preferred:
+            pass
+         elif error_indeterminat:
+            pass
+         elif n_anys < 2:
+            pass
+         else:
+            resultat = clmbo.changeRank('preferred')
+           
     # Constructors, un a partir de la Q i un a partir de la pàgina
     # Ja fan l'item.get perquè així la resta ja poden accedir al contingut
     # de l'item
@@ -139,6 +217,8 @@ def main():
     dades = codecs.open("dades/dades.csv","r")
     lector = csv.reader(dades)
     muni_ant = ""
+    prop_ant = ""
+    canvi_prop = False
     for fila in lector:
        # Saltem les files que no tinguin informació, capçaleres i tal
        # Ho detectem perquè el tercer camp té amplada 5
@@ -149,6 +229,7 @@ def main():
        poblacio = fila[4]
     
        if municipi != muni_ant:     # Canvi de municipi, cal llegir la Q
+          muni_ant = municipi
           try:
             q = dicc_excel[municipi]
           except KeyError:
@@ -156,7 +237,9 @@ def main():
             print "No trobem la Q del municipi "+municipi+" de l'Excel"
             return
           item_muni = wd.wd_item_des_de_q(q)
-          # fem el mateix per les dues propietats
+          nom_muni = wd.wd_nom_municipi()
+          print nom_muni
+          # fem el mateix per les dues propietats per no matxacar info
           anys_omplerts_pob = wd.wd_anys_amb_poblacio('P1082')
           anys_omplerts_fog = wd.wd_anys_amb_poblacio('P1538')
           #print anys_omplerts_pob, anys_omplerts_fog
@@ -164,7 +247,7 @@ def main():
        # anys ja tenen informació. Omplim la resta
 
        if poblacio == '0':
-          print "La poblacio de l'any",any_cens,u" és 0"
+          print "La poblacio de l'any",any_cens,"a ",nom_muni,u" és 0"
           continue
 
         # Per anys < 1700, el que tenim són fogatges (P1538 = nombre de llars)
@@ -173,16 +256,34 @@ def main():
          propietat = 'P1538'
        else:
          propietat = 'P1082'
+         if prop_ant != propietat:  # quan passem de fogatges a població
+            canvi_prop = True
+            # si no fem això, wd_posar_preferit peta. Probablement per un bug
+            # de pywikibot que fa petar el claim.toJSON()
+            # No caldria fer-ho, però així no peta
+            item_muni = wd.wd_item_des_de_q(q)
+
+       # Ara aprofitem que els anys ens vénen per ordre, per arreglar el
+       # camp preferit als fogatges
+       # Posarem el flag de preferit a l'any més alt dels fogatges. Més que
+       # res, perquè hi ha un altre bot que posa aquest flag i de retruc
+       # canvia l'any de julià a gregorià.
+       # Ho podríem fer al final, però és que l'altre bot és molt ràpid
+       if canvi_prop:
+          canvi_prop = False
+          wd.wd_posar_preferit('P1538')
+       prop_ant = propietat
 
        # Si l'any que llegim ja és a Wikidata, saltem la dada
        if any_cens > 1700 and any_cens in anys_omplerts_pob:
-          print "L'any ",any_cens,u"ja és a wikidata"
+          print "L'any ",any_cens,"a ",nom_muni,u"ja és a wikidata"
           continue
        if any_cens <= 1700 and any_cens in anys_omplerts_fog:
-          print "L'any ",any_cens,u"ja és a wikidata"
+          print "L'any ",any_cens,"a ",nom_muni,u"ja és a wikidata"
           continue
 
        wd.wd_actualitzar_pob(propietat,poblacio,any_cens)
+
 
 if __name__ == '__main__':
     try:
